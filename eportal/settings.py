85450d036b1153e0cb26a708de57e55273e0b472
Babalola Peter <ayodelebabalola1004@gmail.com>
2020-06-07 22:03:11 +0100

    fixes security

--- eportal/settings.py
+++ eportal/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('~/Projects/eportal')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!!fum%*$(y_w$-j-sjl=wd&qy+_%phky6rueji4xqye8)3#^&k'
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- eportal/wsgi.py
+++ eportal/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eportal.settings')
+project_folder = os.path.expanduser('~/Projects/eportal')
+load_dotenv(os.path.join(project_folder, '.env'))
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eportal.settings')
 application = get_wsgi_application()
