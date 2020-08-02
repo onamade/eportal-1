fdf746c470c309c86d7498c3d5db8bb9abacd580
Peter Babalola <babalolapeter@icloud.com>
2020-08-02 22:44:40 +0000

    worked--

--- eportal/settings/pro.py
+++ eportal/settings/pro.py
@@ -1,19 +1,17 @@
 from .base import *
 
 DEBUG = False
-ADMINS = (
-    ('Peter B', 'shell.appointment@gmail.com')
-)
+
+ADMINS = [(
+    ('Peter B', 'shell.appointment@gmail.com'),
+)]
 
 ALLOWED_HOSTS = ['*']
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'eportal',
-        'USER': 'eportal',
-        'PASSWORD': os.environ['PASSWORD'],
-        'HOST': os.environ['HOST'],
-        'PORT': os.environ['PORT'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
